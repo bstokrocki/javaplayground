e935eb18a18142915659663b7f0baefab12cc8d0
rgumo <tony.stepfree@yahoo.co.uk>
2020-07-20 21:06:49 +0200

    no message

--- src/test/java/collections/ArrayUtilsTests.java
+++ src/test/java/collections/ArrayUtilsTests.java
@@ -9,34 +9,34 @@ public class ArrayUtilsTests {
 
     @Before
     public void setup() {
-//        utils = ArrayUtils();
+        utils = ArrayUtils();
     }
 
     @Test
     public void testSum() {
-        Assert.assertEquals(24, new int[] {1, 3, 7, 4, 1, 8});
-        Assert.assertEquals(3, new int[] {3});
-        Assert.assertEquals(0, new int[] {-8, 3, 5});
+        Assert.assertEquals(24, utils.sum(new int[] {1, 3, 7, 4, 1, 8}));
+        Assert.assertEquals(3, utils.sum(new int[] {3}));
+        Assert.assertEquals(0, utils.sum(new int[] {-8, 3, 5}));
     }
 
     @Test
     public void testFindLargestNumber() {
-        Assert.assertEquals(8, new int[] {1, 3, 7, 4, 1, 8, -4});
-        Assert.assertEquals(3, new int[] {3});
-        Assert.assertEquals(5, new int[] {-8, 3, 5});
+        Assert.assertEquals(8, utils.findLargestNumber(new int[] {1, 3, 7, 4, 1, 8, -4}));
+        Assert.assertEquals(3, utils.findLargestNumber(new int[] {3}));
+        Assert.assertEquals(5, utils.findLargestNumber(new int[] {-8, 3, 5}));
     }
 
     @Test
     public void testCalculateAverage() {
-        Assert.assertEquals(2.3, new double[] {1.2, 3.1, 7.8, 4.0, -4.6});
-        Assert.assertEquals(3.0, new double[] {3.0});
-        Assert.assertEquals(0.0, new double[] {-8.5, 3.2, 5.3});
+        Assert.assertEquals(2.3, utils.calculateAverage(new double[] {1.2, 3.1, 7.8, 4.0, -4.6}));
+        Assert.assertEquals(3.0, utils.calculateAverage(new double[] {3.0}));
+        Assert.assertEquals(0.0, utils.calculateAverage(new double[] {-8.5, 3.2, 5.3}));
     }
 
     @Test
     public void testJoin() {
-        Assert.assertEquals("XARW", new String[] {"X", "AR", "W"});
-        Assert.assertEquals("QuickBrownFox", new String[] {"Quick", "Brown", "Fox"});
+        Assert.assertEquals("XARW", utils.join(new String[] {"X", "AR", "W"}));
+        Assert.assertEquals("QuickBrownFox", utils.join(new String[] {"Quick", "Brown", "Fox"}));
     }
 
     @Test
