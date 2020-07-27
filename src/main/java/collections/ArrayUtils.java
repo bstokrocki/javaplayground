063dc6dca6e4667d76973f0d77ea1d1809b72796
rgumo <tony.stepfree@yahoo.co.uk>
2020-07-27 22:43:35 +0200

    no message

--- src/main/java/collections/ArrayUtils.java
+++ src/main/java/collections/ArrayUtils.java
@@ -1,4 +1,49 @@
 package collections;
 
-public class ArrayUtils {
+public class ArrayUtils implements IArrayUtils {
+
+
+    @Override
+    public int sum(int[] numbers) {
+
+        int [] suma = new int[] {1, 3, 7, 4, 1, 8};
+        int sum = 0;
+
+        for (int i=0; i<suma.length;i++){
+
+            sum=sum+suma [i];
+
+//            System.out.println(sum);
+        }
+        return 0;
+
+        //sum=sum +suma[i];
+
+        //int [] tablica = new int [1];
+        //int [] tablica1 = new int [3];
+        //int [] tablica2 = new int [7];
+        //int [] tablica3 = new int [4];
+        //int [] tablica4 = new int [1];
+        //int [] tablica5 = new int [8];
+    }
+
+    @Override
+    public int findLargestNumber(int[] numbers) {
+        return 0;
+    }
+
+    @Override
+    public double calculateAverage(double[] numbers) {
+        return 0;
+    }
+
+    @Override
+    public String join(String[] texts) {
+        return null;
+    }
+
+    @Override
+    public int[] takeFirstTwoNumbers(int[] numbers) {
+        return new int[0];
+    }
 }
